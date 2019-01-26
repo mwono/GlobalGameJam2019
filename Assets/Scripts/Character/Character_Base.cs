beae5edb13cc5cf58df9840ea4b0f91499cd7bd7
enganeer13 <zacharydeigert@gmail.com>
2019-01-26 07:23:07 -0800

    no message

---
+++ Assets/Scripts/Character/Character_Base.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character_Base : MonoBehaviour
+{
+
+    public float speed;
+    public float jumpForce;
+    public float moveInput;
+
+    //protected Health health;
+
+    public Rigidbody2D rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        moveInput = Input.GetAxis("Horizontal");
+        rigidbody.velocity = new Vector2(moveInput * speed, rigidbody.velocity.y);
+
+    }
+}
+ 
---
+++ Assets/Scripts/Character/Player_Movement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement : Character_Base
+{
+
+    public float speed;
+    public float jumpForce;
+    public float moveInput;
+
+
+
+    private Rigidbody2D rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+
+    void FixedUpdate()
+    {
+
+        moveInput = Input.GetAxis("Horizontal");
+        
+    }
+
+
+}
