01b4836014564704b6a9d210a6287811cee3ff8a
mwono <michaelwono@gmail.com>
2019-01-27 15:39:04 -0800

    Final Cutscene

--- Assets/Scripts/Dialogue/Cutscene2Manager.cs
+++ Assets/Scripts/Dialogue/Cutscene2Manager.cs
@@ -6,37 +6,49 @@ using UnityEngine.UI;
 public class Cutscene2Manager : MonoBehaviour
 {
     public Image im1, im2, im3;
+    public GameObject princess, castleIntBG;
     // Start is called before the first frame update
     void Start()
     {
-        im1.enabled = true;
-        im2.enabled = true;
         im3.enabled = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        StartCoroutine("im1Fade");
-        StartCoroutine("im2Fade");
-        StartCoroutine("im3Fade");
+        //if (im1.enabled)
+        //{
+        //    StartCoroutine("im1Fade");
+        //}
+        //else if (im2.enabled)
+        //{
+        //    StartCoroutine("im2Fade");
+        //}
+        //else if (im3.enabled)
+        //{
+        //    StartCoroutine("im3Fade");
+        //}
     }
 
     IEnumerator im1Fade()
     {
         im1.CrossFadeAlpha(0f, 1f, false);
         yield return new WaitForSeconds(1f);
+        im2.enabled = true;
     }
 
     IEnumerator im2Fade()
     {
         im2.CrossFadeAlpha(0f, 1f, false);
         yield return new WaitForSeconds(1f);
+        im3.enabled = true;
     }
 
     IEnumerator im3Fade()
     {
         im3.CrossFadeAlpha(0f, 1.5f, false);
-        yield return new WaitForSeconds(1.5f);
+        yield return new WaitForSeconds(1f);
+        princess.SetActive(false);
+        castleIntBG.SetActive(false);
     }
 }
