90cc05fec4504b6424e60394cf9fc55c5f1154f7
themanmann <themanmann@gmail.com>
2019-01-27 14:51:30 -0800

    Animation for Wizard and Princess object, with dummy script to show how to control it

---
+++ Assets/Sprites/WizAndCess/PrincessTest.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrincessTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.anyKeyDown)
+        {
+            this.GetComponent<Animator>().SetInteger("frame", (this.GetComponent<Animator>().GetInteger("frame") + 1) % 3);
+        }
+    }
+}
