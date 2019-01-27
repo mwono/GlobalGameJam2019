02cd91b9fdda32d082620f0ff39e1d39707ce96b
enganeer13 <zacharydeigert@gmail.com>
2019-01-27 00:59:27 -0800

    Running Animation

---
+++ Assets/Scripts/UI/MainMenuSelect.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenuSelect : MonoBehaviour
+{
+
+    public void LoadGame()
+    {
+        SceneManager.LoadScene("MVP");
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void Credits()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+
+    public void Options()
+    {
+        SceneManager.LoadScene("Options");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void LoadLevel(int number)
+    {
+        SceneManager.LoadScene("Level-" + number);
+    }
+    public void PlayAgain()
+    {
+        //play most recent scene
+    }
+}
