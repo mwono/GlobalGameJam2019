58c6182d2a05c09fff5f70b404d700a2e6656c94
themanmann <themanmann@gmail.com>
2019-01-26 18:40:48 -0800

    no message

--- Assets/Scripts/EnemyDetection.cs
+++ Assets/Scripts/EnemyDetection.cs
@@ -11,7 +11,7 @@ public class EnemyDetection : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        this.GetComponent<BoxCollider2D>().enabled = false;
+        
     }
 
     // Update is called once per frame
