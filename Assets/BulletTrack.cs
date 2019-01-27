b4b5d3dfc581f35211a477e66d66af7cdd6897bb
TJMaya97 <tjmaya97@gmail.com>
2019-01-26 18:41:58 -0800

    Merge branch 'ashleykang' of https://github.com/mwono/GlobalGameJam2019 into Ashley
    # Conflicts:
    #	Assets/Scripts.meta

---
+++ Assets/BulletTrack.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletTrack : MonoBehaviour
+{
+    public Transform playerTrans;
+    public float speed = 3;
+
+    private GameObject player;
+    private float timer;
+
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+        playerTrans = player.GetComponent<Transform>();
+        timer = 0;
+
+        Vector2 playerPos = new Vector2(playerTrans.position.x, playerTrans.position.y + 1);
+
+        transform.rotation = Quaternion.LookRotation(playerPos);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += transform.forward * speed * Time.deltaTime;
+        timer += 1 * Time.deltaTime;
+        if ( timer >= 8)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/EnemyProj.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyProj : MonoBehaviour
+{
+    public GameObject bullet;
+    public float fireRate = 2f;
+
+    private float nextFire;
+    private Vector3 lastPos;
+
+    void Start()
+    {
+        nextFire = Time.time;
+    }
+
+    void Update()
+    {
+        CheckIfTimeToFire();
+    }
+
+    void CheckIfTimeToFire()
+    {
+        if (Time.time > nextFire)
+        {
+            Instantiate(bullet, transform.position, Quaternion.identity);
+            nextFire = Time.time + fireRate;
+        }
+    }
+}
