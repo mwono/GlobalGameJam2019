45d5501c0827365637a595635a4dbd75bc1b8588
mwono <michaelwono@gmail.com>
2019-01-29 16:22:06 -0800

    Updated cutscene2

--- Assets/Scripts/Dialogue/Cutscene2Manager.cs
+++ Assets/Scripts/Dialogue/Cutscene2Manager.cs
@@ -6,49 +6,26 @@ using UnityEngine.UI;
 public class Cutscene2Manager : MonoBehaviour
 {
     public Image im1, im2, im3;
-    public GameObject princess, castleIntBG;
+    public GameObject princess, castleIntBG, player;
     // Start is called before the first frame update
     void Start()
     {
-        im3.enabled = true;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        //if (im1.enabled)
-        //{
-        //    StartCoroutine("im1Fade");
-        //}
-        //else if (im2.enabled)
-        //{
-        //    StartCoroutine("im2Fade");
-        //}
-        //else if (im3.enabled)
-        //{
-        //    StartCoroutine("im3Fade");
-        //}
-    }
-
-    IEnumerator im1Fade()
-    {
-        im1.CrossFadeAlpha(0f, 1f, false);
-        yield return new WaitForSeconds(1f);
-        im2.enabled = true;
-    }
-
-    IEnumerator im2Fade()
-    {
-        im2.CrossFadeAlpha(0f, 1f, false);
-        yield return new WaitForSeconds(1f);
-        im3.enabled = true;
-    }
-
-    IEnumerator im3Fade()
-    {
-        im3.CrossFadeAlpha(0f, 1.5f, false);
-        yield return new WaitForSeconds(1f);
         princess.SetActive(false);
         castleIntBG.SetActive(false);
+        player.SetActive(false);
+        StartCoroutine("Fade");
+    }
+
+    IEnumerator Fade()
+    {
+        im1.enabled = true;
+        im2.enabled = true;
+        im3.enabled = true;
+        im1.CrossFadeAlpha(0f, 3f, false);
+        yield return new WaitForSeconds(3f);
+        im1.enabled = false;
+        im2.CrossFadeAlpha(0f, 3f, false);
+        yield return new WaitForSeconds(3f);
+        im2.enabled = false;
     }
 }
